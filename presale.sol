de0e5f51b59eeeaa19c6a1bf1d0bf693bb2755d9
NightFury0109 <nightfury0109@gmail.com>
2022-04-18 17:57:28 +0200

    [complete]: token, node management, presale

---
+++ presale.sol
@@ -0,0 +1,287 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.7.5;
+
+interface IERC20 {
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address account) external view returns (uint256);
+
+  function transfer(address recipient, uint256 amount) external returns (bool);
+
+  function allowance(address owner, address spender)
+    external
+    view
+    returns (uint256);
+
+  function approve(address spender, uint256 amount) external returns (bool);
+  function burn(uint256 amount) external;
+
+  function transferFrom(
+    address sender,
+    address recipient,
+    uint256 amount
+  ) external returns (bool);
+
+
+  function mintPRESALE(address account_, uint256 amount_) external;
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+library SafeMath {
+  function add(uint256 a, uint256 b) internal pure returns (uint256) {
+    uint256 c = a + b;
+    require(c >= a, "SafeMath: addition overflow");
+
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+    return sub(a, b, "SafeMath: subtraction overflow");
+  }
+
+  function sub(
+    uint256 a,
+    uint256 b,
+    string memory errorMessage
+  ) internal pure returns (uint256) {
+    require(b <= a, errorMessage);
+    uint256 c = a - b;
+
+    return c;
+  }
+
+  function mul(uint256 a, uint256 b) internal pure returns (uint256) {
+    if (a == 0) {
+      return 0;
+    }
+
+    uint256 c = a * b;
+    require(c / a == b, "SafeMath: multiplication overflow");
+
+    return c;
+  }
+
+  function div(uint256 a, uint256 b) internal pure returns (uint256) {
+    return div(a, b, "SafeMath: division by zero");
+  }
+
+  function div(
+    uint256 a,
+    uint256 b,
+    string memory errorMessage
+  ) internal pure returns (uint256) {
+    require(b > 0, errorMessage);
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+
+    return c;
+  }
+}
+
+library Address {
+  function isContract(address account) internal view returns (bool) {
+    uint256 size;
+    // solhint-disable-next-line no-inline-assembly
+    assembly {
+      size := extcodesize(account)
+    }
+    return size > 0;
+  }
+
+  function functionCall(
+    address target,
+    bytes memory data,
+    string memory errorMessage
+  ) internal returns (bytes memory) {
+    return _functionCallWithValue(target, data, 0, errorMessage);
+  }
+
+  function _functionCallWithValue(
+    address target,
+    bytes memory data,
+    uint256 weiValue,
+    string memory errorMessage
+  ) private returns (bytes memory) {
+    require(isContract(target), "Address: call to non-contract");
+
+    // solhint-disable-next-line avoid-low-level-calls
+    (bool success, bytes memory returndata) = target.call{value: weiValue}(
+      data
+    );
+    if (success) {
+      return returndata;
+    } else {
+      if (returndata.length > 0) {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+          let returndata_size := mload(returndata)
+          revert(add(32, returndata), returndata_size)
+        }
+      } else {
+        revert(errorMessage);
+      }
+    }
+  }
+
+  function _verifyCallResult(
+    bool success,
+    bytes memory returndata,
+    string memory errorMessage
+  ) private pure returns (bytes memory) {
+    if (success) {
+      return returndata;
+    } else {
+      if (returndata.length > 0) {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+          let returndata_size := mload(returndata)
+          revert(add(32, returndata), returndata_size)
+        }
+      } else {
+        revert(errorMessage);
+      }
+    }
+  }
+}
+
+library SafeERC20 {
+  using SafeMath for uint256;
+  using Address for address;
+
+  function safeTransfer(
+    IERC20 token,
+    address to,
+    uint256 value
+  ) internal {
+    _callOptionalReturn(
+      token,
+      abi.encodeWithSelector(token.transfer.selector, to, value)
+    );
+  }
+
+  function safeTransferFrom(
+    IERC20 token,
+    address from,
+    address to,
+    uint256 value
+  ) internal {
+    _callOptionalReturn(
+      token,
+      abi.encodeWithSelector(token.transferFrom.selector, from, to, value)
+    );
+  }
+
+  function _callOptionalReturn(IERC20 token, bytes memory data) private {
+    bytes memory returndata = address(token).functionCall(
+      data,
+      "SafeERC20: low-level call failed"
+    );
+    if (returndata.length > 0) {
+      // Return data is optional
+      // solhint-disable-next-line max-line-length
+      require(
+        abi.decode(returndata, (bool)),
+        "SafeERC20: ERC20 operation did not succeed"
+      );
+    }
+  }
+}
+
+interface IOwnable {
+  function owner() external view returns (address);
+
+  function renounceOwnership() external;
+
+  function transferOwnership(address newOwner_) external;
+}
+
+contract Ownable is IOwnable {
+  address internal _owner;
+
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+  constructor() {
+    _owner = msg.sender;
+    emit OwnershipTransferred(address(0), _owner);
+  }
+
+  function owner() public view override returns (address) {
+    return _owner;
+  }
+
+  modifier onlyOwner() {
+    require(_owner == msg.sender, "Ownable: caller is not the owner");
+    _;
+  }
+
+  function renounceOwnership() public virtual override onlyOwner {
+    emit OwnershipTransferred(_owner, address(0));
+    _owner = address(0);
+  }
+
+  function transferOwnership(address newOwner_)
+    public
+    virtual
+    override
+    onlyOwner
+  {
+    require(newOwner_ != address(0), "Ownable: new owner is the zero address");
+    emit OwnershipTransferred(_owner, newOwner_);
+    _owner = newOwner_;
+  }
+}
+
+interface IMythor {
+    function exchangeWithNode(uint kind, uint amount, string memory name, address account) external;
+}
+
+contract Presale is Ownable {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  IMythor public  token;
+  uint[3] public prices = [0, 0, 0];
+  mapping(address=>mapping(uint=>uint)) tokenForUsers;
+
+  IERC20 public buyToken;
+
+  uint256 public startTime;
+  uint256 public endTime;
+
+  constructor(IMythor _token, IERC20 _buyToken) {
+      token = _token;
+      buyToken = _buyToken;
+  }
+  function initialize(uint[3] memory _prices, uint256 _startTime, uint256 _endTime) external onlyOwner {
+      prices = _prices;
+      startTime = _startTime;
+      endTime = _endTime;
+  }
+  function purchase(uint kind, uint amount) external {
+      require(block.timestamp >= startTime && block.timestamp <= endTime, "Not started");
+      require(address(buyToken) != address(0), "invalid buyToken");
+      require(kind < 3, "invalid kind");
+      require(prices[kind] > 0, "invalid balance");
+      uint256 totalAmount = prices[kind].mul(amount);
+      buyToken.transferFrom(msg.sender, address(this), totalAmount);
+      tokenForUsers[msg.sender][kind] += amount;
+  }
+  function exchangeWithNode(uint kind, string memory name) external {      
+      require(block.timestamp > endTime, "Not ended");
+      require(tokenForUsers[msg.sender][kind] >= 1, "not enough balance");
+      token.exchangeWithNode(kind, 1, name, msg.sender);
+      tokenForUsers[msg.sender][kind]--;
+  }
+  function withDraw(address account) external onlyOwner{
+      payable(account).transfer(payable(address(this)).balance);
+  }
+  function withDraw(address account, IERC20 token) external onlyOwner{
+      token.transfer(account, token.balanceOf(address(this)));
+  }
+}
